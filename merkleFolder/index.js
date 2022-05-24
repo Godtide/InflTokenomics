afbe3ba9f5b7ef27ef624aff7b90d994cadb754c
“Godtide” <ayoadebabatide@gmail.com>
2022-05-25 00:55:18 +0100

    keccak256 merkle proof

--- merkleFolder/index.js
+++ merkleFolder/index.js
@@ -3,17 +3,7 @@ const { MerkleTree } = require('merkletreejs')
 
 const { keccak256 } = require("@ethersproject/solidity");
 
- const whitelist = [
-  "0x4c17cF5Ee96Ca430d1Dd14EfCe4385e1e162B556",
-
-  "0x2169B5deeD138e7828cf69D3d8e4fD54c469cdFb",
-
-  "0x81964e06cA51F7426F17c52485f9f5B0bA446F02",
-
-  "0x9816d41ea5132f9cf8cb53aa8cbfea5b8faab85d",
-
-  "0x5f98Bf2254BF20F70f1ca7722abBa28359591deD",
-];
+ const whitelist = ["address1", "address2"];
 
   function hashAccount(address) {
   return keccak256(["address"], [address]);
@@ -31,8 +21,8 @@ const { keccak256 } = require("@ethersproject/solidity");
 
 
      const tree = getMerkleTree();
-    const hexProof = tree.getHexProof(hashAccount("0x81964e06cA51F7426F17c52485f9f5B0bA446F02"));
-    const leaf = hashAccount("0x81964e06cA51F7426F17c52485f9f5B0bA446F02");
+    const hexProof = tree.getHexProof(hashAccount("address1"));
+    const leaf = hashAccount("address1");
     const hexRoot = tree.getHexRoot().toString("hex");
     const verify = tree.verify(hexProof, leaf, hexRoot);
     console.log("hex root",hexRoot);
